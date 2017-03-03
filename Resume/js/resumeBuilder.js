d4a7ceb060758cf229ae4be8508a9a1f74cbe04a
Jainil Vachhani <jainilvachhani@gmail.com>
2017-03-04 00:06:54 +0530

    Add files via upload

---
+++ Resume/js/resumeBuilder.js
@@ -0,0 +1,67 @@
+var bio = 
+{
+	"name" : "Jainil Vachhani",
+	"role" : "Software Developer",
+	"contacts" :
+	{
+		"mobile" : "XXX-XXX-XXXX",
+		"email" : "jainilvachhani@gmail.com",
+		"github" : "jainilvachhani",
+		"twitter" : "@jainilvachhani",
+		"location" : "ahmedabad"
+	},
+	"welcomeMessage" : "hello there",
+	"skills":
+	["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"],
+	"bioPic" : "images/fry.jpg"
+}
+
+var education = 
+{
+	"schools" : 
+	[
+	{
+		"name" : "School of Engineering and Applied Sciences",
+		"city" : "Ahmedabad",
+		"degree" : "Bachelors",
+		"majors" : ["ICT"],
+		"dates" : 2018,
+		"url" : "http://example.com"
+	}
+	],
+	"onlineCourses" : 
+	[
+		{
+			"title" : "JavaScript Crash Course",
+			"school" : "Udacity",
+			"dates" : 2017,
+			"url" : "http://www.udacity.com/course/ud804"
+		}
+	]	
+}
+
+var work = 
+{
+	"jobs" :
+	[
+		{
+			"employer" : "Indian Institute of Technology",
+			"title" : "Research intern",
+			"dates" : "May 2016 - July 2016",
+			"description" : "worked as research intern"
+		}
+	]
+}
+
+var projects = 
+{
+	"projects" :
+	[
+		{
+			"title" : "Sample project 1",
+			"dates" : "2014",
+			"description" : "who moved my cheese",
+			"images" : ["http://google.com","http://udacity.com"]
+		}
+	]
+}
